d3286fe75c4c6c6cc3cbcc74ab545aab63708c64
Kirill <kirill.trumpeter@gmail.com>
2023-11-26 10:00:57 +0300

    Изменён индикатор активности

--- app/src/main/java/com/example/cryptocurrencyapp/presentation/coin_list/components/CoinListItem.kt
+++ app/src/main/java/com/example/cryptocurrencyapp/presentation/coin_list/components/CoinListItem.kt
@@ -3,7 +3,7 @@ package com.example.cryptocurrencyapp.presentation.coin_list.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-н import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
